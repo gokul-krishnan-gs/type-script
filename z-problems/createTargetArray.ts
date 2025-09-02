e4e80f10d9dc42a8723605f208198679f88c74e9
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-09-02 22:38:09 +0530

    Create createTargetArray.ts

---
+++ z-problems/createTargetArray.ts
@@ -0,0 +1,8 @@
+const createTargetArray = (nums: number[], index: number[]): number[] => {
+    let target: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        // Insert nums[i] at position index[i]
+        target.splice(index[i], 0, nums[i]);
+    }
+    return target;
+};
